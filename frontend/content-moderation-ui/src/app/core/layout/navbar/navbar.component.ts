b5a62c05a0e7cc9d43238e5a24a15687d982d47e
ahmed ferjani <ferjaniahmed1997@gmail.com>
2026-06-03 03:42:44 +0100

    feat(frontend): introduce navbar component

---
+++ frontend/content-moderation-ui/src/app/core/layout/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {}

1c4a520f7c2913d078f10ec152f406a91a6504a3
ahmed ferjani <ferjaniahmed1997@gmail.com>
2026-06-09 02:49:59 +0100

    feat(frontend): add footer component

--- frontend/content-moderation-ui/src/app/app.config.ts
+++ frontend/content-moderation-ui/src/app/app.config.ts
@@ -1,13 +1,28 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideBrowserGlobalErrorListeners, provideEnvironmentInitializer } from '@angular/core';
+import { inject, type ApplicationConfig } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import type { ApplicationConfig } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withFetch()),
     provideRouter(routes),
+    provideEnvironmentInitializer(() => {
+      const iconRegistry = inject(MatIconRegistry);
+      const sanitizer = inject(DomSanitizer);
+
+      iconRegistry.addSvgIcon(
+        'github',
+        sanitizer.bypassSecurityTrustResourceUrl('icons/github.svg'),
+      );
+      iconRegistry.addSvgIcon(
+        'linkedin',
+        sanitizer.bypassSecurityTrustResourceUrl('icons/linkedin.svg'),
+      );
+    }),
   ],
 };
--- frontend/content-moderation-ui/src/app/core/layout/shell/shell.component.ts
+++ frontend/content-moderation-ui/src/app/core/layout/shell/shell.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-shell',
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, FooterComponent, RouterOutlet],
   templateUrl: './shell.component.html',
   styleUrl: './shell.component.scss',
 })

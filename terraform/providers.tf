f577b442d3ce46a7c0346d9e1362dbf3e095bc1f
ahmed ferjani <ferjaniahmed1997@gmail.com>
2026-06-12 01:12:31 +0100

    chore(terraform): add default AWS resource tags

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,12 @@
 provider "aws" {
   region = var.aws_region
 
+  default_tags {
+    tags = {
+      Project     = "content-moderation"
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Owner       = "Ahmed Ferjani"
+    }
+  }
 }

3542bad86a5e89c3459e779916f563176de26d96
ahmed ferjani <ferjaniahmed1997@gmail.com>
2026-06-24 02:04:06 +0100

    feat(infra): add health route

--- terraform/modules/api-gateway/main.tf
+++ terraform/modules/api-gateway/main.tf
@@ -39,6 +39,12 @@ resource "aws_apigatewayv2_integration" "this" {
   payload_format_version = "2.0"
 }
 
+resource "aws_apigatewayv2_route" "get_health" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "GET /health"
+  target    = "integrations/${aws_apigatewayv2_integration.this.id}"
+}
+
 resource "aws_apigatewayv2_route" "post_uploads" {
   api_id    = aws_apigatewayv2_api.this.id
   route_key = "POST /uploads"

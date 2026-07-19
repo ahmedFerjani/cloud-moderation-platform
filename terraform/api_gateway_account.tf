f016cabb2497632eb2dbc89e88148de6943705be
ahmed ferjani <ferjaniahmed1997@gmail.com>
2026-07-19 07:33:31 +0100

    feat(infra:cloudwatch): create account-level cloudwatchlogs role for API Gatway access logging

---
+++ terraform/api_gateway_account.tf
@@ -0,0 +1,23 @@
+# Account-level CloudWatch Logs role for API Gateway access logging
+# Required for WebSocket APIs; HTTP APIs don't need it
+resource "aws_iam_role" "this" {
+  name = "${local.name_prefix}-apigateway-cloudwatch-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "apigateway.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
+resource "aws_api_gateway_account" "this" {
+  cloudwatch_role_arn = aws_iam_role.this.arn
+}
